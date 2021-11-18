de38991185905ace2ea233bee88814eff8062a05
Ghost1t <lor.lor@mail.ru>
2021-11-18 15:30:03 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 84);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать коллекцию";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(344, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(444, 289);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 84);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохрать коллекцию в файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Элементов в коллекции";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(193, 130);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Создание файла с коллекцией";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly List<int> numbers = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Button1_Click(object sender, EventArgs e)
+        {
+            numbers.Clear();
+            textBox2.Clear();
+
+            int Kol = Convert.ToInt32(numericUpDown1.Value);
+
+            for (int i = 0; i <= Kol-1; i++) // Запись в коллекцию произвольных чисел.
+            {
+                int value = new Random().Next();
+                numbers.Add(value);
+            }
+
+
+            for (int i = 0; i < numbers.Count; i++) // Запись коллекции  в textBox2.
+            {
+
+                textBox2.Text += numbers[i].ToString() + "\r\n";
+
+            }
+
+            return;
+        }
+
+
+        void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        void Button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt"; // Расширение файла.
+            saveFileDialog1.FilterIndex = 2; // Получает или задает индекс фильтра, выбранного в данный момент в диалоговом окне файла.
+            saveFileDialog1.RestoreDirectory = true; // Восстанавливает ли диалоговое окно каталог в ранее выбранный каталог перед закрытием.
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllLines(saveFileDialog1.FileName, numbers.ConvertAll(x => x.ToString())); // Запись в файл "FileName" коллекции numbers.
+            }
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
